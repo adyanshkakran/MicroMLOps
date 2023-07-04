47e8acf1d8494a819648944662f5638b0cd7b503
Shrikara A <shrikarame@gmail.com>
2023-07-05 05:11:58 +0530

    created microservice template

---
+++ template.py
@@ -0,0 +1,109 @@
+"""
+Template for microservices
+Reads config from an input topic, 
+performs operations and then
+produces message on output topic
+"""
+
+import os
+from kafka import KafkaConsumer, KafkaProducer
+from dotenv import load_dotenv
+
+load_dotenv()
+
+"""
+configuring kafka
+these must be set in env
+default input topic is filename (without .py)
+output topic must be specified
+"""
+input_topic:str = os.environ.get("INPUT_TOPIC", os.path.basename(__file__)[:-3])
+output_topic:str = os.environ.get("OUTPUT_TOPIC", "default_output_topic")
+kafka_broker:str = os.environ.get("KAFKA_BROKER", "localhost:9092")
+consumer_group_id:str = os.environ.get("KCON_GROUP_ID", "default_group_id")
+
+if os.environ.get("MICROML_DEBUG", "0"):
+    print(f"Input Topic: {input_topic}; Output Topic: {output_topic}")
+    print(f"Group ID: {consumer_group_id}; Kafka Broker: {kafka_broker}")
+
+def setup_kafka_consumer():
+    """
+    Create and return a kafka consumer
+    Uses global variables for config
+    Uses specified input_topic, group_id, kafka_broker
+    """
+    config = {
+        "group_id": consumer_group_id,
+        "bootstrap_servers": kafka_broker
+    }
+    try:
+        consumer = KafkaConsumer(input_topic, **config)
+        return consumer
+    except Exception as e:
+        raise Exception("Failed to create Kafka Consumer")
+
+
+def setup_kafka_producer():
+    """
+    Create and return a kafka producer
+    Uses global variables for config
+    Uses specified kafka_broker as bootstrap_server
+    """
+    config = {
+        "bootstrap_servers": kafka_broker
+    }
+    try:
+        producer = KafkaProducer(**config)
+        return producer
+    except Exception as e:
+        raise Exception("Failed to create Kafka Producer")
+
+
+#######
+def read_and_execute(consumer: KafkaConsumer, producer: KafkaProducer):
+    """
+    Consumer listens for messages from input queue
+    When it reads a message, processes it 
+    Then producer sends output 
+    """
+    try:
+        for message in consumer:
+            output_message = process_message(message)
+            send_message(output_message, producer)
+    except KeyboardInterrupt:
+        print("Exiting...")
+
+def process_message(message):
+    """
+    Read incoming message
+    Do necessary computation
+    Return output message
+    """
+    print("received message: ", message)
+    print("do computation here")
+    return "output_message"
+
+def send_message(output_message, producer: KafkaProducer):
+    """
+    Send output message to output_topic
+    """
+    print(f"format {output_message} for sending")
+    producer.send(output_topic, b"output message here")
+
+    
+#######
+def main():
+    consumer = setup_kafka_consumer()
+    producer = setup_kafka_producer()
+
+    try:
+        read_and_execute(consumer, producer)
+    except Exception as e:
+        print(e)
+    finally:
+        consumer.close()
+        producer.flush()
+
+
+if __name__ == "__main__":
+    main()
