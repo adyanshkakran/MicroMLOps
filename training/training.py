37f6989bac14b52d5fa1352fba04546fb9bbaaf7
Shrikara A <shrikarame@gmail.com>
2023-07-06 12:10:03 +0530

    Added Training File(No functionality)
    Added training.py using template. Calls {model} function.

---
+++ training/training.py
@@ -0,0 +1,135 @@
+"""
+Template for microservices
+Reads config from an input topic, 
+performs operations and then
+produces message on output topic
+"""
+
+import os
+import json
+import numpy as np
+import pandas as pd
+from kafka import KafkaConsumer, KafkaProducer
+from dotenv import load_dotenv
+from pprint import pprint
+
+from svm import svm
+from random_forest import random_forest
+
+load_dotenv(override=True) # env file has higher preference
+
+"""
+configuring kafka
+these must be set in env
+default input topic is filename (without .py)
+output topic must be specified
+"""
+input_topic:str = os.environ.get("INPUT_TOPIC", os.path.basename(__file__)[:-3])
+output_topic:str = os.environ.get("OUTPUT_TOPIC", "default_output_topic")
+kafka_broker:str = os.environ.get("KAFKA_BROKER", "localhost:9092")
+consumer_group_id:str = os.environ.get("KCON_GROUP_ID", "default_group_id")
+
+if os.environ.get("MICROML_DEBUG", "0"):
+    print(f"Input Topic: {input_topic}; Output Topic: {output_topic}")
+    print(f"Group ID: {consumer_group_id}; Kafka Broker: {kafka_broker}")
+
+def setup_kafka_consumer():
+    """
+    Create and return a kafka consumer
+    Uses global variables for config
+    Uses specified input_topic, group_id, kafka_broker
+    """
+    config = {
+        "group_id": consumer_group_id,
+        "bootstrap_servers": kafka_broker
+    }
+    try:
+        consumer = KafkaConsumer(input_topic, **config)
+        return consumer
+    except Exception as e:
+        raise Exception("Failed to create Kafka Consumer")
+
+
+def setup_kafka_producer():
+    """
+    Create and return a kafka producer
+    Uses global variables for config
+    Uses specified kafka_broker as bootstrap_server
+    """
+    config = {
+        "bootstrap_servers": kafka_broker
+    }
+    try:
+        producer = KafkaProducer(value_serializer=lambda v: json.dumps(v).encode('utf-8'), **config)
+        return producer
+    except Exception as e:
+        raise Exception("Failed to create Kafka Producer")
+
+
+#######
+def read_and_execute(consumer: KafkaConsumer, producer: KafkaProducer):
+    """
+    Consumer listens for messages from input queue
+    When it reads a message, processes it 
+    Then producer sends output 
+    """
+    try:
+        for message in consumer:
+            output_message = process_message(message)
+            send_message(output_message, producer)
+    except KeyboardInterrupt:
+        print("Exiting...")
+
+def process_message(message):
+    """
+    Read incoming message
+    Do necessary computation
+    Return output message
+    """
+    message_obj = json.loads(message.value)
+    if os.environ.get("MICROML_DEBUG", "0"):
+        pprint(message_obj)
+    
+    model_config = message_obj["model"]
+    training_config = message_obj["training"]
+    data = pd.read_csv(message_obj["data"])
+    job_uuid = message_obj["uuid"]
+
+    if not model_config or not training_config:
+        raise Exception("Model or training config not specified")
+    
+    execute(data, model_config, message_obj, job_uuid)
+    
+    return message_obj
+
+def execute(data: pd.DataFrame, model_config: str, config: dict, job_uuid: str):
+    try:
+        globals()[model_config](data, config, job_uuid)
+    except Exception as e:
+        print(e)
+
+def send_message(output_message, producer: KafkaProducer):
+    """
+    Send output message to output_topic
+    """
+    if os.environ.get("MICROML_DEBUG", "0"):
+        print("format output message for sending")
+    producer.send(output_topic, output_message)
+
+    
+#######
+def main():
+    consumer = setup_kafka_consumer()
+    producer = setup_kafka_producer()
+
+    try:
+        read_and_execute(consumer, producer)
+    except Exception as e:
+        print(e)
+    finally:
+        consumer.close()
+        producer.flush()
+
+
+if __name__ == "__main__":
+    main()
