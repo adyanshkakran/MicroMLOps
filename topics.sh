c8476b5dbe53f56c5fc9948e896816c087863a22
Shrikara A <shrikarame@gmail.com>
2023-07-07 11:56:48 +0530

    bootstrap server variable

--- topics.sh
+++ topics.sh
@@ -1,6 +1,7 @@
-bin/kafka-topics.sh --create --topic data_preprocessing --if-not-exists --bootstrap-server localhost:9092
-bin/kafka-topics.sh --create --topic feature_extraction --if-not-exists --bootstrap-server localhost:9092
-bin/kafka-topics.sh --create --topic training --if-not-exists --bootstrap-server localhost:9092
-bin/kafka-topics.sh --create --topic model_management --if-not-exists --bootstrap-server localhost:9092
-bin/kafka-topics.sh --create --topic result --if-not-exists --bootstrap-server localhost:9092
-bin/kafka-topics.sh --create --topic inference --if-not-exists --bootstrap-server localhost:9092
+KAFKA_SERVER=localhost:9092
+bin/kafka-topics.sh --create --topic data_preprocessing --if-not-exists --bootstrap-server ${KAFKA_SERVER}
+bin/kafka-topics.sh --create --topic feature_extraction --if-not-exists --bootstrap-server ${KAFKA_SERVER}
+bin/kafka-topics.sh --create --topic training --if-not-exists --bootstrap-server ${KAFKA_SERVER}
+bin/kafka-topics.sh --create --topic model_management --if-not-exists --bootstrap-server ${KAFKA_SERVER}
+bin/kafka-topics.sh --create --topic result --if-not-exists --bootstrap-server ${KAFKA_SERVER}
+bin/kafka-topics.sh --create --topic inference --if-not-exists --bootstrap-server ${KAFKA_SERVER}
