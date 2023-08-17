e53f396af5fd85140bc1020793fb0ad3deed04c2
Shrikara A <shrikarame@gmail.com>
2023-08-17 19:36:12 +0530

    [Test] Offline and Online Pipelines

--- topics.sh
+++ topics.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 KAFKA_SERVER=localhost:9092
-/bin/kafka-topics --create --topic data_preprocessing --if-not-exists --bootstrap-server ${KAFKA_SERVER}
-/bin/kafka-topics --create --topic feature_extraction --if-not-exists --bootstrap-server ${KAFKA_SERVER}
-/bin/kafka-topics --create --topic training --if-not-exists --bootstrap-server ${KAFKA_SERVER}
-/bin/kafka-topics --create --topic model_management --if-not-exists --bootstrap-server ${KAFKA_SERVER}
-/bin/kafka-topics --create --topic result --if-not-exists --bootstrap-server ${KAFKA_SERVER}
-/bin/kafka-topics --create --topic inference --if-not-exists --bootstrap-server ${KAFKA_SERVER}
+topics=("data_preprocessing" "feature_extraction" "model_management" "result")
+modes=("training" "inference")
 /bin/kafka-topics --create --topic logs --if-not-exists --bootstrap-server ${KAFKA_SERVER}
+/bin/kafka-topics --create --topic training --if-not-exists --bootstrap-server ${KAFKA_SERVER}
+/bin/kafka-topics --create --topic inference --if-not-exists --bootstrap-server ${KAFKA_SERVER}
+
+for topic in ${topics[@]}; do
+    for mode in ${modes[@]}; do
+        echo "${mode}_${topic}"
+        /bin/kafka-topics --create --topic ${mode}_${topic} --if-not-exists --bootstrap-server ${KAFKA_SERVER}
+    done 
+done
